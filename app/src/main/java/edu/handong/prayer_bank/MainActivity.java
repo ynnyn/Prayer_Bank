1b63e89b96cd7d9351a187b1a9c732d5e9f4f856
sehee <kimsehee7414@naver.com>
2022-10-12 00:56:37 +0900

    please,,,

--- app/src/main/java/edu/handong/prayer_bank/MainActivity.java
+++ app/src/main/java/edu/handong/prayer_bank/MainActivity.java
@@ -2,7 +2,10 @@ package edu.handong.prayer_bank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //make the main button
+        ImageButton praybutton = findViewById(R.id.prayButton);
+        ImageButton mypagebutton = findViewById(R.id.mypageButton);
+        ImageButton summarybutton = findViewById(R.id.summaryButton);
+        ImageButton calenderbutton = findViewById(R.id.calendarButton);
+        ImageButton homebutton = findViewById(R.id.homeButton);
+
+        //screen change
+        //main -> pray screen
+        praybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pray = new Intent(MainActivity.this, Prayer.class);
+                startActivity(pray);
+            }
+        });
+        //main -> my page screen
+        mypagebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mypage = new Intent(MainActivity.this, MyPage.class);
+                startActivity(mypage);
+            }
+        });
+        //main -> summary screen
+        summarybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent summary = new Intent(MainActivity.this, Summary.class);
+                startActivity(summary);
+            }
+        });
+        //main -> calender
+        calenderbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent calender = new Intent(MainActivity.this, Calendar.class);
+                startActivity(calender);
+            }
+        });
+        //main -> home
+        homebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent home = new Intent(MainActivity.this, MainActivity.this);
+            }
+        });
+
+
+
     }
 }
