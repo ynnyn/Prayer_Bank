5438793b48ecbae4b98695d66ce5aa29e872711c
ynnyn <101929276+ynnyn@users.noreply.github.com>
2022-10-25 23:01:08 +0900

    .

--- app/src/main/java/edu/handong/prayer_bank/MainActivity.java
+++ app/src/main/java/edu/handong/prayer_bank/MainActivity.java
@@ -2,24 +2,89 @@ package edu.handong.prayer_bank;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import com.android.car.ui.toolbar.MenuItem;
 import com.google.android.material.navigation.NavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout drawer;
+    ActionBarDrawerToggle toggle;
+    boolean isDrawerOpend;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton buttonOpen = findViewById(R.id.menuButton);
+        buttonOpen.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.category_drawer_view);
+                if (drawer.isDrawerOpen(Gravity.LEFT)){
+                    drawer.closeDrawer(Gravity.LEFT);
+                }
+            }
+        });
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        drawer = (DrawerLayout) findViewById(R.id.category_drawer_view);
+        toggle = new ActionBarDrawerToggle(this, drawer, R.string.drawer_open, R.string.drawer_close){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+            @Override
+            public boolean onOptionsItemSelected(MenuItem item) {
+                if (toggle.onOptionsItemSelected(item)) {
+                    return true;
+                }
+                return super.onOptionsItemSelected(item);
+            }
+        };
+        drawer.addDrawerListener(toggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView)
+                findViewById(R.id.category_drawer_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            @SuppressWarnings("StatementWithEmptyBody")
+            @Override
+            public boolean onNavigationItemSelected(MenuItem Item) {
+                int id=Item.getItemId();
+
+                if(id==R.id.nav_me) {
+                    showToast("NavigationDrawer...me..");
+                } else if(id == R.id.nav_family) {
+                    showToast("NavigationDrawer...family..");
+                } else if(id == R.id.nav_friends) {
+                    showToast("NavigationDrawer...friends..");
+                }
+
+                return false;
+            }
+
+
+        });
 
         //make the main button
         ImageButton praybutton = findViewById(R.id.prayButton);
@@ -67,45 +132,16 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
-    implements NavigationView.OnNavigationItemSelectedListener {
-        @Override
-                protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle (
-                    this,drawer,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
-            //drawer.setDrawerListener(toggle);
-            drawer.addDrawerListener((DrawerLayout.DrawerListener) toggle);
-            toggle.syncState();
-
-            NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-            navigationView.setNavigationItemSelectedListener(this);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return false;
         }
-        @Override
-                public void onBackPressed() {
-            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-            if (drawer.isDrawerOpen(GravityCompat.START)) {
-                drawer.closeDrawer(GravityCompat.START);
-            } else {
-                super.onBackPressed();
-            }
-        }
+        return super.onOptionsItemSelected(item);
+    }
+    private void showToast(String message) {
+        Toast toast=Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
 
-        @SuppressWarnings("StatementWithEmptyBody")
-                @Override
-        public boolean onNavigationItemSelected(MenuItem item) {
-            //Handle navigation viw item clicks here.
-            int id = int.getItemId();
-
-            if (id == R.id.nav_me) {
-            } else if (id == R.id.nav_family) {
-            } else if (id == R.id.nav_friends) {
-            }
-            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-            drawer.closeDrawer(GravityCompat.START);
-            return true;
-
-        }
 }
