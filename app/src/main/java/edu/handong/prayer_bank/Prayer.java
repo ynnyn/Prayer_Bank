091807b36c18cf54fc4b2e1db45cdcdd0dcc4a61
ynnyn <101929276+ynnyn@users.noreply.github.com>
2022-10-25 23:02:57 +0900

    .

--- app/src/main/java/edu/handong/prayer_bank/Prayer.java
+++ app/src/main/java/edu/handong/prayer_bank/Prayer.java
@@ -2,18 +2,36 @@ package edu.handong.prayer_bank;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class Prayer extends AppCompatActivity {
 
+    private int seconds = 0;
+    private boolean running;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prayer);
+
+        Button startButton = findViewById(R.id.start_button);
+        startButton.setOnClickListener(view -> {
+            running = true;
+        });
+        Button stopButton = findViewById(R.id.stop_button);
+        stopButton.setOnClickListener(view -> {
+            running = false;
+        });
+        runTimer();
+
 
         //make the main button
         ImageButton praybutton = findViewById(R.id.prayButton);
@@ -64,4 +82,24 @@ public class Prayer extends AppCompatActivity {
         });
 
     }
+
+    private void runTimer() {
+        final TextView timeView = findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format("%d:%02d:%02d",hours,minutes,secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this,1000);
+            }
+        });
+    }
+
 }
